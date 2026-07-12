c5149b786f8f17d5ae0489d0ae28c57a13912ed5
Ishant Tikhe <tikheishant@gmail.com>
2026-07-12 22:41:56 +0530

    Add delay.h header file for delay functions

---
+++ MINI_PROJECT/inc/delay.h
@@ -0,0 +1,11 @@
+//-------------------delay.h-------------------
+#ifndef DELAY_H
+#define DELAY_H
+
+#include "types.h"
+
+void delay_us(u32 i);
+void delay_ms(u32 i);
+void delay_s(u32 i);
+
+#endif // DELAY_H
