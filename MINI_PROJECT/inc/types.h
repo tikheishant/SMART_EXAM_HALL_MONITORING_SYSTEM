838efa2ea36d41536a461463b341ef5d76babcdc
Ishant Tikhe <tikheishant@gmail.com>
2026-07-12 22:51:10 +0530

    Add types.h with basic type definitions

---
+++ MINI_PROJECT/inc/types.h
@@ -0,0 +1,11 @@
+//-------------------types.h-------------------
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char u8;
+typedef char           s8;
+typedef unsigned int   u32;
+typedef int             s32;
+typedef float         f32;
+
+#endif // TYPES_H
