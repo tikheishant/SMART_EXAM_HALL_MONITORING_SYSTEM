3a3e119a0a23d24fd66aa6231777152c1b5cb8aa
Ishant Tikhe <tikheishant@gmail.com>
2026-07-12 22:44:01 +0530

    Add kpm_defines.h with keypad pin definitions

---
+++ MINI_PROJECT/inc/kpm_defines.h
@@ -0,0 +1,18 @@
+//kpm_defines.h
+#ifndef KPM_DEFINES_H
+#define KPM_DEFINES_H
+
+// row cfg pins
+#define ROW0 16
+#define ROW1 17
+#define ROW2 18
+#define ROW3 19
+// column pins
+#define COL0 20
+#define COL1 21
+#define COL2 22
+#define COL3 23
+
+#endif // KPM_DEFINES_H
+
+
