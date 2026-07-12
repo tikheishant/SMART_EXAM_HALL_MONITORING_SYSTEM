f2bdeec22cedb1503812120c6b43781538810034
Ishant Tikhe <tikheishant@gmail.com>
2026-07-12 22:50:43 +0530

    Add segment defines header file

---
+++ MINI_PROJECT/inc/seg_defines.h
@@ -0,0 +1,9 @@
+//seg_defines.h
+#ifndef SEG_DEFINES_H
+#define SEG_DEFINES_H
+
+#define ca7seg_2_mux 24 // p1.24 - p1.31
+#define DSEL1        19 // p0.19
+#define DSEL2        20 // p0.20
+
+#endif // SEG_DEFINES_H
