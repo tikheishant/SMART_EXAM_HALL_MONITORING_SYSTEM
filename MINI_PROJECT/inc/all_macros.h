036d9ea35efaa61e1e4a7fc97e77cb2a1d2c9617
Ishant Tikhe <tikheishant@gmail.com>
2026-07-12 22:40:40 +0530

    Add all_macros.h header file with includes

---
+++ MINI_PROJECT/inc/all_macros.h
@@ -0,0 +1,17 @@
+//all_macros.h
+#ifndef ALL_MACROS_H
+#define ALL_MACROS_H
+
+#include <LPC21xx.h>
+#include <stdlib.h>
+
+#include "types.h"
+#include "defines.h"
+#include "rtc_defines.h"
+#include "lcd_defines.h"
+#include "kpm_defines.h"
+#include "seg_defines.h"
+#include "adc_defines.h"
+#include "io_defines.h"
+
+#endif // ALL_MACROS_H
